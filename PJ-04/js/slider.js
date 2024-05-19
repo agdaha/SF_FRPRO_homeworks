2e7467998c4f5182c0e300890dfb03cd98ed296f
Rishat Tarisov <r.tarisov@gmail.com>
2024-05-19 23:19:34 +0500

    feat: before js

---
+++ PJ-04/js/slider.js
@@ -0,0 +1,32 @@
+let slides =[
+    {
+        url: 
+            `../img/image1.png`,
+        text: `Only a small part of the work performed by our company is presented on the site. For 14 years on in
+        the construction market we have made happy more than 1000 families`,
+        city: "Rostov-on-Don<br>LCD admiral",
+        area: "81 m2",
+        reptime: "3.5 months",
+        cosr: "Upon request",
+    },
+    {
+        url: 
+            `../img/image2.png`,
+        text: `Only a small part of the work performed by our company is presented on the site. For 14 years on in the construction 
+        market we have made happy more than 1000 families`,
+        city: "Sochi<br>Thieves",
+        area: "105 m2",
+        reptime: "4 months",
+        cosr: "Upon request",
+    },
+    {
+        url: 
+            `../img/image3.png`,
+        text: `Only a small part of the work performed by our company is presented on the site. For 14 years on in the construction 
+        market we have made happy more than 1000 families`,
+        city: "Rostov-on-Don<br>Patriotic",
+        area: "93 m2",
+        reptime: "3 months",
+        cosr: "Upon request",
+    },
+];
