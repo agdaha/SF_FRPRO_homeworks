bcbf7e0b0f2106abec1e6bed4d843431c46e7e8a
Rishat Tarisov <r.tarisov@gmail.com>
2024-05-21 09:56:54 +0500

    Update task2.js

--- module14/task2.js
+++ module14/task2.js
@@ -1,1 +1,36 @@
+const jsonString = `
+{
+ "list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}
+`;
 
+const data = JSON.parse(jsonString);
+
+const persons = data.list;
+
+let result = {
+  list :[ ]
+}
+
+persons.forEach((person) => {
+  result.list.push(
+    {
+      name: person.name, 
+      age: person.age, 
+      prof: person.prof
+    }
+  )
+});
+
+console.log(result);
